95da8a7bdd02a3562fa74b242cc7fc49e82712ae
Emi <emisato02@gmail.com>
2017-04-13 22:17:41 -0400

    changes

--- src/script.js
+++ src/script.js
@@ -162,6 +162,174 @@ $("#cs-home-scroll-down").click(function() {
 
 
 
+
+
+
+// SHOPPING CART //
+// ---------------cart item add--------------------------------
+
+$('.add-cart').click(function(){
+    var currentCount = parseInt($(this).parent().find('.count').text());
+    var totalPrice = parseInt($('.total span').text());
+
+    var price = parseInt($(this).parent().find(".price span").text());
+    var title = $(this).parent().find('.product-name').text();
+    var find = title.replace(/\s/g, '');
+    var imagePlace = $(this).parent().parent().find('img').attr('src');
+    var totalItemNumber = parseInt($('.totalItemNumber').text());
+
+    currentCount = currentCount + 1;
+    totalItemNumber = totalItemNumber +1;
+    totalPrice = totalPrice + price;
+
+
+    $('.total span').text(totalPrice);
+
+
+
+    $('.thumTotalPrice').text(totalPrice)
+    $(this).parent().find('.count').text(currentCount);
+    $(this).parent().parent().addClass('itemClicked');
+
+
+    $('.totalItemNumber').text(totalItemNumber);
+
+    if (currentCount > 1) {
+        $('.checkout ul').find('.'+find).remove();
+        $('.checkout ul').prepend('<li class ="'+find+'"><img class="thumImage" src="'+imagePlace+'" alt="place holder image"><p class="thumTitle">'+title+'</p><div >$<span class="thumPrice">'+price+'</span> x<span class="thumCount">'+currentCount+'</span></div></li>');
+
+    }else{
+        $('.checkout ul').prepend('<li class ="'+find+'"><img class="thumImage" src="'+imagePlace+'" alt="place holder image"><p class="thumTitle">'+title+'</p><div >$<span class="thumPrice">'+price+'</span> x<span class="thumCount">'+currentCount+'</span></div></li>');
+    }
+    if (totalPrice >0){
+        $(".empty").hide();
+    }else{}
+
+});
+
+
+// ---------------cart item minus--------------------------------
+$('.min-cart').click(function(){
+    var currentCount = parseInt($(this).parent().find('.count').text());
+    var totalPrice = parseInt($('.total span').text());
+    var price = parseInt($(this).parent().find(".price span").text());
+    var title = $(this).parent().find('.product-name').text();
+    var find = title.replace(/\s/g, '');
+    var imagePlace = $(this).parent().parent().find('img').attr('src');
+    var totalItemNumber = parseInt($('.totalItemNumber').text());
+
+
+    if (currentCount == 1) {
+
+        currentCount = currentCount - 1;
+        totalPrice = totalPrice - price;
+        totalItemNumber = totalItemNumber -1;
+        $('.total span').text(totalPrice);
+        $(".thumTotalPrice").text(totalPrice)
+        $(this).parent().find('.count').text(currentCount);
+        $('.checkout ul').find('.'+find).remove();
+        $('.totalItemNumber').text(totalItemNumber);
+        $(this).parent().parent().removeClass('itemClicked');
+
+    }else if(currentCount>1){
+        currentCount = currentCount - 1;
+        totalPrice = totalPrice - price;
+        totalItemNumber = totalItemNumber -1;
+        $('.totalItemNumber').text(totalItemNumber);
+        $('.total span').text(totalPrice);
+        $(".thumTotalPrice").text(totalPrice)
+        $(this).parent().find('.count').text(currentCount);
+        $('.checkout ul').find('.'+find).remove();
+        $('.checkout ul').prepend('<li class ="'+find+'"><img class="thumImage" src="'+imagePlace+'" alt="place holder image"><p class="thumTitle">'+title+'</p><div >$<span class="thumPrice">'+price+'</span> x(<span class="thumCount">'+currentCount+'</span>)</div></li>');
+    }else{}
+
+    if (totalPrice ==0){
+        $(".empty").show();
+    }else{}
+
+});
+
+// ---------------Check Out--------------------------------
+$('.myAccount').click(function(){
+    // $('.checkout').slideDown(1000);
+    // $('.checkoutBlack').slideDown(1000);
+
+    TweenMax.to('.checkout',0.8,{
+        transform:"translateY(100px)",
+        display:"block",
+        opacity:"1",
+        // delay:0.8
+    });
+    TweenMax.to('.checkoutBlack',0.8,{
+        display:"block",
+        opacity:"1",
+        // delay:0.8
+    });
+
+});
+
+
+// ---continue shopping-------
+$('.checkout h3').click(function(){
+    // $('.checkout').slideUp(1000);
+    // $('.checkoutBlack').slideUp(1000);
+
+    TweenMax.to('.checkout',0.8,{
+        transform:"translateY(0px)",
+        display:"none",
+        opacity:"0",
+    });
+
+    TweenMax.to('.checkoutBlack',0.8,{
+
+        display:"none",
+        opacity:"0",
+        delay:0.5
+    });
+
+
+
+})
+
+// ------place order---------
+$('.checkoutBtn').click(function(){
+    var totalPrice = parseInt($('.total span').text());
+    if (totalPrice > 0){
+
+        TweenMax.to('.beforeCheckout',0.5,{
+            display:"none",
+            opacity:"0",
+        });
+        TweenMax.to('.afterCheckout',2.0,{
+            display:"block",
+            opacity:"1",
+            delay:0.5
+        });
+
+
+    }else{}
+});
+
+// ----------Close Checkout-------------
+$('.backBtn').click(function(){
+    // $('.checkout').slideUp(1000);
+    // $('.checkoutBlack').slideUp(1000);
+
+    TweenMax.to('.checkout',0.8,{
+        transform:"translateY(0px)",
+        display:"none",
+        opacity:"0",
+    });
+
+    TweenMax.to('.checkoutBlack',0.8,{
+
+        display:"none",
+        opacity:"0",
+        delay:0.5
+    });
+});
+
+
 /********************** Mobile Menu ***********************/
 'use strict';
 
